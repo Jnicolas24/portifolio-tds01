a901fae7c06c2becdf2cab24e131bd9b7633ba2e
joão <joaonsarruda@gmail.com>
2025-02-18 11:53:31 -0300

    changes js

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let telefone = document.getElementById("telefone");
 let mensagem = document.getElementById ("mensagem");
 let paragrafo = document.getElementById("mensagemObt");
 
+emailjs.init('7GPJIbyNNwyI3obd0'); //inicializando o emailjs com API_KEY
+
 // innerText adiciona um texto no html atraves do js
 // paragrafo.innerText = "Exemplo de texto";
 
@@ -17,13 +19,21 @@ function verificarCampos(event) {
         paragrafo.innerText = "Preencha todos os campos!";
         paragrafo.style.color = "red";
         paragrafo.style.fontWeight = "bold";
-        paragrafo.style.textAlign = "center"
+        paragrafo.style.textAlign = "center";
     } else {
-        paragrafo.innerText = "Formulário enviado com sucesso!"
+        paragrafo.innerText = "Formulário enviado com sucesso!";
         paragrafo.style.color = "#00ff00";
-        paragrafo.style.fontWeight = "bold"
-        paragrafo.style.textAlign = "center"
-    }
+        paragrafo.style.fontWeight = "bold";
+        paragrafo.style.textAlign = "center";
 
+        emailjs.send('service_u7htro3', 'template_qy2v8mh',
+            {
+                to_name: nome.value,
+                to_email: email.value,
+                to_tel: telefone.value,
+                to_message: mensagem.value
+            }
+        );
+    }
 }
 
